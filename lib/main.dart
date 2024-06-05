f4cac97f9b278120406241eb0e6f1c0da69c2412
khanhkk96 <vudinhkhanh7777@gmail.com>
2024-06-05 09:27:39 +0700

    khanhkk_update notfication

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -38,32 +39,48 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum NotifyType {
+  SUCCESS,
+  WARNING,
+  ERROR,
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   List<XFile> images = [];
 
+  void notify(String message, NotifyType type){
+    var mapTypes = <NotifyType,Color>{
+      NotifyType.SUCCESS: Colors.blue,
+      NotifyType.WARNING: Colors.orange,
+      NotifyType.ERROR: Colors.red,
+    };
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Center(
+            child: Wrap(children: [
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child:  Text(
+                    message,
+                    style: TextStyle(
+                        color: mapTypes[type],
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.none),
+                  )),
+            ]),
+          );
+        });
+  }
+
   Future<void> _generatePdf() async {
     if (images.isEmpty) {
-      showDialog(
-          context: context,
-          builder: (context) {
-            return Center(
-              child: Wrap(children: [
-                Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Text(
-                      "Chưa chọn hình ảnh!",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.none),
-                    )),
-              ]),
-            );
-          });
+      notify("Chưa chọn hình ảnh!", NotifyType.ERROR);
       return;
     }
 
@@ -83,36 +100,15 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
-    // String filename = DateTime.now().millisecondsSinceEpoch.toString();
     final DateTime now = DateTime.now();
-    final DateFormat formatter = DateFormat('yyyy-MM-dd_HH-mm-ss');
+    final DateFormat formatter = DateFormat('yyyy-MM-dd_HH-mm-ss_ms');
     final String filename = formatter.format(now);
     final file =
         File('${Directory('/storage/emulated/0/Download').path}/$filename.pdf');
     // print("result: ${file.path}");
     await file.writeAsBytes(await pdf.save());
 
-    showDialog(
-        context: context,
-        builder: (context) {
-          return Center(
-            child: Wrap(children: [
-              Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  child: const Text(
-                    "Tạo file pdf thành công.",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.none),
-                  )),
-            ]),
-          );
-        });
+    notify("Tạo file pdf thành công.", NotifyType.SUCCESS);
   }
 
   Future<void> _pickImages() async {
@@ -128,18 +124,6 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      // body: Center(
-      //   child: GridView.builder(
-      //       itemCount: images.length,
-      //       itemBuilder: (ctx,index) {
-      //         return Image.file(File(images[index].path));
-      //       }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     crossAxisCount: 1,
-      //     mainAxisSpacing: 10,
-      //     crossAxisSpacing: 5,
-      //     childAspectRatio: 1.0
-      //   ))
-      // ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -178,7 +162,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 1.0,
                     )),
                   ),
-                  child: const Text("Chọn hình"),
+                  child: const Text("Chọn hình ảnh"),
                 ),
               ))
         ],
